f54b29460df7a837cf98f0f877ddf013bfcc789d
Pipat Saengow <pay2630@gmail.com>
2020-04-09 19:06:50 +0700

    network, client: Entity Interpolation

--- core/src/com/progmethgame/client/ClientEntity.java
+++ core/src/com/progmethgame/client/ClientEntity.java
@@ -4,7 +4,9 @@ import java.util.UUID;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
 import com.progmethgame.common.DisplayType;
+import com.progmethgame.common.GameConfig;
 import com.progmethgame.network.EntityData;
 
 public class ClientEntity extends Sprite{
@@ -12,11 +14,18 @@ public class ClientEntity extends Sprite{
 	private UUID gid;
 	private DisplayType type;
 	private ClientRuntime runtime;
+	private Vector2[] posBuf;
+	private long[] timeBuf;
 	
 	public ClientEntity(UUID gid, DisplayType type, ClientRuntime runtime) {
 		this.gid = gid;
 		this.type = type;
 		this.runtime = runtime;
+		this.posBuf = new Vector2[2];
+		this.posBuf[0] = new Vector2();
+		this.posBuf[1] = new Vector2();
+		this.timeBuf = new long[2];
+		this.timeBuf[0] = this.timeBuf[1] = System.currentTimeMillis();
 		
 		updateTexture();
 	}
@@ -44,9 +53,26 @@ public class ClientEntity extends Sprite{
 	public void update(EntityData data) {
 		this.gid = data.id;
 		this.type = data.dispType;
-		this.setPosition(data.position.x, data.position.y);
+		posBuf[0].set(posBuf[1]);
+		posBuf[1].set(data.position);
+		timeBuf[0] = timeBuf[1];
+		timeBuf[1] = System.currentTimeMillis();
 		
 		updateTexture();
+	}
+	
+	public void tick(float delta) {
+		Vector2 pos = posBuf[0].cpy().lerp(
+				posBuf[1],
+				Math.max(-1,
+						Math.min(1,
+								((float) ( System.currentTimeMillis() - GameConfig.CLIENT_ENTITY_INTERPOLATION_TIME_MILLIS - timeBuf[0]))/
+								(timeBuf[1] - timeBuf[0]+1)
+								)
+						));
+		setPosition(pos.x,  pos.y);
+		//System.out.println((System.currentTimeMillis() - GameConfig.CLIENT_ENTITY_INTERPOLATION_TIME_MILLIS - timeBuf[0] ) + " " + (timeBuf[1] - timeBuf[0]));
+	//	System.out.println("Rendering X Y T X Y Y " + posBuf[0] + " " + timeBuf[0] + " " +posBuf[1] + " " + timeBuf[1] + " T " + System.currentTimeMillis());
 	}
 	
 	public static ClientEntity fromData(EntityData data, ClientRuntime runtime) {
--- core/src/com/progmethgame/common/GameConfig.java
+++ core/src/com/progmethgame/common/GameConfig.java
@@ -2,5 +2,5 @@ package com.progmethgame.common;
 
 public final class GameConfig {
 	public static float SERVER_TICK_RATE = 1/30f;
-	public static float CLIENT_TICK_RATE = 1/20f;
+	public static int CLIENT_ENTITY_INTERPOLATION_TIME_MILLIS = (int) (SERVER_TICK_RATE *1000);
 }
