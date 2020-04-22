bd2e200ccc1ee2bca53591c9996c76bec53e02d0
Pipat Saengow <pay2630@gmail.com>
2020-04-22 16:50:14 +0700

    clint.screen.basestage: Fix gamescreen stretching

--- core/src/com/progmethgame/client/screen/BaseStage.java
+++ core/src/com/progmethgame/client/screen/BaseStage.java
@@ -3,11 +3,13 @@ package com.progmethgame.client.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class BaseStage extends Stage {
 	protected Skin skin;
 
 	public BaseStage() {
+		super(new FitViewport(500, 500));
 		this.skin = new Skin(Gdx.files.internal("plain-james/skin/plain-james-ui.json"));
 	}
 }
