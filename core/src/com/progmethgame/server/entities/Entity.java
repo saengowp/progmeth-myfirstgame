d4b51b75fd8076f3a962b42dfea95a79a8b110db
Pipat Saengow <pay2630@gmail.com>
2020-05-07 23:19:57 +0700

    doc: more doc on entity

--- core/src/com/progmethgame/server/entities/Entity.java
+++ core/src/com/progmethgame/server/entities/Entity.java
@@ -14,14 +14,26 @@ import com.progmethgame.server.blocks.Block;
  */
 public abstract class Entity implements Tickable {
 
+	/** Position */
 	protected Vector2 position;
+	
+	/** Velocity */
 	protected Vector2 velocity;
 
+	/** Displayed type*/
 	protected DisplayType type;
+	
+	/** ID */
 	protected final UUID gid;
 	
+	/** Overlays attached */
 	protected ArrayList<Overlay> overlays;
 
+	/**
+	 * Create a new entity with specified ID and type
+	 * @param gid
+	 * @param type
+	 */
 	public Entity(UUID gid, DisplayType type) {
 		this.gid = gid;
 		this.type = type;
@@ -30,6 +42,10 @@ public abstract class Entity implements Tickable {
 		this.overlays = new ArrayList<Overlay>();
 	}
 	
+	/**
+	 * Create a new entity with specified type and a random id
+	 * @param type
+	 */
 	public Entity(DisplayType type) {
 		this(UUID.randomUUID(), type);
 	}
@@ -44,6 +60,10 @@ public abstract class Entity implements Tickable {
 		return gid.hashCode();
 	}
 	
+	/**
+	 * Change the display type
+	 * @param t
+	 */
 	public void setDisplay(DisplayType t) {
 		this.type = t;
 	}
@@ -53,23 +73,31 @@ public abstract class Entity implements Tickable {
 
 	}
 
+	/**
+	 * Get the velocity
+	 * @return
+	 */
 	public Vector2 getVelocity() {
 		return this.velocity;
 	}
 
+	/** Get the position */
 	public Vector2 getPosition() {
 		return this.position;
 	}
 
+	/** Get the display type */
 	public DisplayType getType() {
 		return type;
 	}
 
+	/** Get the entity's id */
 	public UUID getGid() {
 		return gid;
 	}
 	
-	/** Parse rendering data for GameClient
+	/** 
+	 * Parse the graphical rendering data for sending accross the network 
 	 * 
 	 * @return EntityData
 	 */
@@ -82,32 +110,28 @@ public abstract class Entity implements Tickable {
 		return d;
 	}
 	
-	/** Call when entity touched other.
+	/** 
+	 * Called when entity touched other.
 	 * 
-	 * @param other
+	 * @param other the touched entity
 	 */
 	public void onCollide(Entity other) {
 		
 	}
-	
-	public void setPosition(Vector2 position) {
-		this.position.set(position);
-	}
 
-	public void setVelocity(Vector2 velocity) {
-		this.velocity.set(velocity);
-	}
-
-	/** Call when walked on the block
+	/**
+	 * Called when walked on the block
 	 * 
-	 * @param block
+	 * @param block the block that got walked on
 	 */
 	public void onWalkOn(Block block) {
 		
 	}
 	
-	/** Call when entity hit solid block
+	/** 
+	 * Call when entity hit solid block
 	 * 
+	 * @param block the block that this entity collided with
 	 */
 	public void onCollideSolid(Block block) {
 		
